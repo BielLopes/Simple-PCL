1b1facb81e8734bbfb68fb7929f0a4dd25224af1
Gabriel Lopes Machado <galoma@ufmg.br>
2021-10-05 22:27:50 -0300

    simple code to create point cloud and save as PCD

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Simple-PCL
 Using Point Cloud Library to convert PTS to PCD
+
+## Compile
+<code>mkdir build</code>
+<br/>
+<code>cd build</code>
+<br/>
+<code>cmake ..</code>
+<br/>
+<code>make</code>
---
+++ pcd_write.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+int
+  main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZRGB> cloud;
+
+  // Fill in the cloud data
+  cloud.width    = 5;
+  cloud.height   = 1;
+  cloud.is_dense = false;
+  cloud.points.resize (cloud.width * cloud.height);
+
+  for (auto& point: cloud)
+  {
+    point.x = 1024 * rand () / (RAND_MAX + 1.0f);
+    point.y = 1024 * rand () / (RAND_MAX + 1.0f);
+    point.z = 1024 * rand () / (RAND_MAX + 1.0f);
+    point.r = 0;
+    point.g = 0;
+    point.b = 15;
+    point.a = 0;
+  }
+
+  pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
+  std::cerr << "Saved " << cloud.size () << " data points to test_pcd.pcd." << std::endl;
+
+  for (const auto& point: cloud)
+    std::cerr << "    " << point.x << " " << point.y << " " << point.z << std::endl;
+
+  return (0);
+}
